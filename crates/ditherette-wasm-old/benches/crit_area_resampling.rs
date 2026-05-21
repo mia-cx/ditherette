5c6449f36acb0fd7290c49316b624dd09a4cb12a
mia-riezebos <mia@mia.cx>
2026-05-21 08:03:41 +0200

    fix(wasm-old): update area criterion import

--- crates/ditherette-wasm-old/benches/crit_area_resampling.rs
+++ crates/ditherette-wasm-old/benches/crit_area_resampling.rs
@@ -1,7 +1,7 @@
 use std::{hint::black_box, time::Duration};
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use ditherette_wasm::{
+use ditherette_wasm_old::{
     image::{rgba, ImageDimensions},
     resize::area::resize_rgba_area_into,
 };
